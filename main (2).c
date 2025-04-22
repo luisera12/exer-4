e144055baf0e11e0d203043ac304bbaa54273f49
luisera12 <luisfelipeoliveirasilva14@gmail.com>
2025-04-22 16:59:02 -0300

    Update main (2).c

--- main (2).c
+++ main (2).c
@@ -10,16 +10,16 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 int main()
 {
-    int numeros[6], cubos[6];
+    int numeros[5], cubos[5];
     
-    for (int i = 0; i < 6; i++){
+    for (int i = 0; i < 5; i++){
         printf("Digite um numero %d: ", i+1);
         scanf("%d", &numeros[i]);
         cubos[i] = numeros[i] * numeros[i] * numeros [i];
     }
     
     printf("numero:\n Cubo: \n");
-    for (int i = 0; i < 6; i++){
+    for (int i = 0; i < 5; i++){
         printf("%d\n%d\n", numeros[i], cubos[i]);
     }
 
